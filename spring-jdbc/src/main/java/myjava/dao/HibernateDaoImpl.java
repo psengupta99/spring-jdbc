25e0855f19977229e56c6037eff4ef8f016ea37d
PSG <purnesh.sen@gmail.com>
2019-01-02 22:29:23 +0530

    test branch commit

--- spring-jdbc/src/main/java/myjava/dao/HibernateDaoImpl.java
+++ spring-jdbc/src/main/java/myjava/dao/HibernateDaoImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import myjava.model.Circle;
-//hibernate package..
+//hibernate package..this is just a test
 @Repository(value = "hibernateDao")
 public class HibernateDaoImpl {
 
